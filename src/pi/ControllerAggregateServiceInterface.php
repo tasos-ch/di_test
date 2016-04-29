653f6f76ee6837e512cc47849c87c35c47b62af0
Tasos Chalkias <imtasos@gmail.com>
2016-04-29 19:19:54 +0300

    Update documentation

--- src/pi/ControllerAggregateServiceInterface.php
+++ src/pi/ControllerAggregateServiceInterface.php
@@ -5,6 +5,11 @@ namespace pi;
 
 use Psr\Log\LoggerInterface;
 
+/**
+ * Interface ControllerAggregateServiceInterface
+ * @package pi
+ * @see https://autofac.readthedocs.io/en/latest/advanced/aggregate-services.html
+ */
 interface ControllerAggregateServiceInterface {
 
 	/**
