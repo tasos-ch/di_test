d04948620b488790cbc6e406342bd489e473a432
Tasos Chalkias <imtasos@gmail.com>
2016-04-29 17:52:14 +0300

    Add index.php

---
+++ src/index.php
@@ -0,0 +1,23 @@
+<?php
+
+use pi\ControllerA;
+use pi\ControllerB;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+require_once '../vendor/autoload.php';
+
+
+$dic = new ContainerBuilder();
+$diConfLoader = new YamlFileLoader($dic, new FileLocator('../'));
+$diConfLoader->load('di.yml');
+
+/** @var \pi\DatabaseConnectionInterface $dbConnection */
+$dbConnection = $dic->get('db_connection');
+/** @var LoggerInterface $logger */
+$logger = $dic->get('logger');
+
+$a = new ControllerA($dbConnection, $logger);
+$b = new ControllerB($dbConnection);
