f5e80c0bd35bacd2bbc95185e4c25627a1a8aa96
Tasos Chalkias <imtasos@gmail.com>
2016-05-19 13:55:59 +0300

    Add NewInterface dependency to ControllerA

--- src/pi/ControllerA.php
+++ src/pi/ControllerA.php
@@ -8,10 +8,25 @@ use Psr\Log\LoggerInterface;
 class ControllerA extends BaseController {
 
 	/**
+	 * @var NewInterface
+	 */
+	private $newInterface;
+
+	/**
 	 * ControllerA constructor.
 	 * @inheritdoc
 	 */
-	public function __construct(DatabaseConnectionInterface $dbConnection, LoggerInterface $logger) {
+	public function __construct(
+		DatabaseConnectionInterface $dbConnection,
+		LoggerInterface $logger,
+		NewInterface $newInterface
+	) {
 		parent::__construct($dbConnection, $logger);
+		$this->newInterface = $newInterface;
+	}
+
+	public function index() {
+		// Do stuff...
+		$classC = new ClassC($this->newInterface);
 	}
 }
